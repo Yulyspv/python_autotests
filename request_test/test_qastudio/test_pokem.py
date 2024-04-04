69bd47023184c724c3e277f2b0b501b4b01d26b8
Yulyspv <153379340+Yulyspv@users.noreply.github.com>
2024-04-04 20:19:51 +0300

    Add files via upload

--- request_test/test_qastudio/test_pokem.py
+++ request_test/test_qastudio/test_pokem.py
@@ -1,17 +1,15 @@
 import pytest
 import requests
 
-
-def test_status_code_200():
-    url = 'https://api.pokemonbattle-stage.me/v2/trainers'
-    response = requests.get(url)
-    assert response.status_code == 200
+host='https://api.pokemonbattle-stage.me/v2'
+token='92dec79899b03a3ad84bb2f80bffd945'
 
 
-def test_id916():  
-  
-  response = requests.get ('https://pokemonbattle-stage.me/v2/trainers', params= {'trainer_id': '916'})
-  response = response.json()
-  
-  assert response['data']['trainer_name'] == 'Юлия'
- 
+
+def test_status_code():
+     response=requests.get(f'{host}/trainers', params={'trainer_id':916})
+     assert response.status_code == 200
+
+def test_trainer_name():
+     response=requests.get(f'{host}/trainers', params={'trainer_id':916})
+     assert response.json()["trainer_name"]=="Юлия"
